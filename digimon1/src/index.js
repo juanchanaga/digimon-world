79dcaf35acbc994c01e6ae12b80c76a76542a8db
juanchanaga <105959221+juanchanaga@users.noreply.github.com>
2022-09-12 16:03:56 -0500

    Index (#1)
    Co-authored-by: Juan Pablo <juan.chanaga@artemisa.tech>

---
+++ digimon1/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <HashRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </HashRouter>
+);
